e481b21d9dfe0915e8ac0d25f37d32b4e307358a
JayAree999 <jayyqwe123@gmail.com>
2023-03-21 08:36:59 +0700

    Update everything

---
+++ pages/logout.tsx
@@ -0,0 +1,41 @@
+// pages/logout.js
+import { useEffect } from 'react';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+export default function Logout() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleLogout = () => {
+      const url = 'https://www.spotify.com/logout/';
+      const spotifyLogoutWindow = window.open(
+        url,
+        'Spotify Logout',
+        'width=700,height=500,top=40,left=40'
+      );
+
+      setTimeout(async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        spotifyLogoutWindow.close();
+        await signOut({
+          callbackUrl: `${window.location.origin}/`,
+          redirect: true,
+        });
+      }, 2000);
+    };
+
+    handleLogout();
+  }, [router]);
+
+  return (
+    <div>
+      <h1>Logging out...</h1>
+      <p>
+        Please close the Spotify logout window if it doesn't close
+        automatically.
+      </p>
+    </div>
+  );
+}
