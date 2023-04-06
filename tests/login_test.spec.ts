eb784cb77158f14a8b10866f92fc23d59a6cee0f
BlackTea13 <robertblum2001@gmail.com>
2023-04-07 03:44:31 +0700

    added playlist selecting test

--- tests/login_test.spec.ts
+++ tests/login_test.spec.ts
@@ -1,4 +1,6 @@
 import {test, expect, Page, Locator} from '@playwright/test';
+import {google} from "google-gax/build/protos/locations";
+import location = google.cloud.location;
 
 // Write a playwright test to check for a button
 // with the text "Continue as Guest" and click it.
@@ -15,12 +17,27 @@ class HomePage {
     getContinueAsGuestButton(): Locator {
         return this.page.locator("//button[contains(text(), 'Continue as Guest')]").first()
     }
+
+    getLoginWithSpotifyButton(): Locator {
+        return this.page.locator("//button[contains(text(), 'Log in with Spotify')]").first()
+    }
 }
 
 class PlaylistPage {
     page: Page
     constructor(page: Page) {
         this.page = page;
+    }
+    async navigate(): Promise<void> {
+        await this.page.goto('http://localhost:3000/select-playlist?isGuest=true');
+    }
+
+    selectFirstPlaylist(): Locator {
+        return this.page.locator('//div[@data-testid="playlist-0"]').first()
+    }
+
+    selectFirstPlaylistImage(): Locator {
+        return this.page.locator('//div[@data-testid="playlist-0"]//img').first()
     }
 }
 
@@ -41,5 +58,38 @@ test.describe("Home Page Buttons Test", () => {
         const playlistPage = new PlaylistPage(page)
         await playlistPage.page.waitForLoadState('networkidle')
         await expect(playlistPage.page).toHaveURL(/.*select-playlist.*isGuest=true/)
+    })
+
+    test('Login button should lead to spotify login', async () => {
+        const homePage = new HomePage(page)
+        await homePage.navigate()
+        const loginButton = homePage.getLoginWithSpotifyButton()
+        await expect(loginButton).toBeVisible()
+        await loginButton.click()
+
+        await page.waitForLoadState('networkidle')
+        await expect(page).toHaveURL(/.*spotify.com/)
+    })
+})
+
+test.describe("Playlist Page selection tests", () => {
+    let page: Page
+
+    test.beforeAll(async ({browser}) => {
+        page = await browser.newPage()
+    })
+
+    test('selecting a playlist should show the songs in the playlist', async () => {
+        const playlistPage = new PlaylistPage(page)
+        await playlistPage.navigate()
+
+        const playlistDiv = page.getByTestId("playlist-0").first()
+        const playlistImg = playlistPage.selectFirstPlaylistImage()
+        const playlistName = await playlistImg.getAttribute("alt")
+
+        await playlistDiv.click()
+        // @ts-ignore
+        await expect(page.getByText(playlistName)).toBeVisible()
+
     })
 })
