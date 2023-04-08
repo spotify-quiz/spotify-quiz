4c5155a8b4db2cf70e90a512f2167319be5445cf
Rathanon <rathanon.sin@student.mahidol.edu>
2023-04-08 21:09:38 +0700

    redesign pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/Login.module.css';
 
+
 const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 const REDIRECT_URI = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI as string;
 const CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID as string;
 const SCOPE = 'user-library-read playlist-read-private';
+
+
 
 export default function Login() {
   const router = useRouter();
@@ -38,7 +41,7 @@ export default function Login() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to My Spotify App</h1>
+      <img src ="./images/Quizme.png" width={800} height={800}/>
       <button className={styles.button} onClick={handleLogin}>
         Log in with Spotify
       </button>
